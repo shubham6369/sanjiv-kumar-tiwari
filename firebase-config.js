7ab74913a3924f1cae64a606f9331fa9948df741
shubham6369 <shubham67257@gmail.com>
2026-02-27 00:05:51 +0530

    Update Firebase config for Storage support

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getFirestore, collection, addDoc, getDocs, onSnapshot, query, orderBy, updateDoc, doc, deleteDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCD-_Hl3yzvtk1sADLv0Bxr5UwK21_GW4g",
-    authDomain: "sanjeevkumar-10e39.firebaseapp.com",
-    projectId: "sanjeevkumar-10e39",
-    storageBucket: "sanjeevkumar-10e39.firebasestorage.app",
-    messagingSenderId: "308312312401",
-    appId: "1:308312312401:web:0b272be1fba7c83150c670",
-    measurementId: "G-FMW6F7D1M5"
+    apiKey: "AIzaSyDRtf6Nl_rYM3S_TXSo3Fx-MwbNLf6RLR8",
+    authDomain: "sanjeevkumar-2beff.firebaseapp.com",
+    projectId: "sanjeevkumar-2beff",
+    storageBucket: "sanjeevkumar-2beff.firebasestorage.app",
+    messagingSenderId: "179014952526",
+    appId: "1:179014952526:web:8f24e7475a879ac6b8051f",
+    measurementId: "G-S9T0B6NP05"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const storage = getStorage(app);
 
-export { db, collection, addDoc, getDocs, onSnapshot, query, orderBy, updateDoc, doc, deleteDoc };
+export { db, collection, addDoc, getDocs, onSnapshot, query, orderBy, updateDoc, doc, deleteDoc, storage, ref, uploadBytesResumable, getDownloadURL };
